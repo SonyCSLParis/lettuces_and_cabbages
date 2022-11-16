f2bbc16057d100c868376c3932f981827e66f5fa
kodda <koddda@gmail.com>
2022-11-16 09:30:04 +0000

    1D merged

--- 1D/simulations.py
+++ 1D/simulations.py
@@ -3,6 +3,7 @@ import time
 import intercropsim as ics
 import utils as ut
 import numpy as np
+from joblib import Parallel, delayed
 
 def one_run_test():
    np.random.seed(123456)
@@ -23,8 +24,22 @@ def one_run(pr, sim_params, name = "test", svg_folder = "res/", plot_folder = "f
    res = ics.sim(sim_params, sim_params["smart"],  sim_params["waste"], svg_folder+name+".json")
    sim_data = json.load(open(svg_folder+name+".json"))
    ut.plot_field(sim_data["plants"], sim_data["sim_params"], [0,8000], plot_folder+name+".png")
-   print(time.time()-t0, "s for pr = %.2f"%pr)
-   return [pr,ts]
+   print(time.time()-t0, "s for pr = %.2f %s %s"%(pr,sim_params["smart"],sim_params["waste"]))
+   return 0
+
+def one_run_j(j, prs, sim_params, svg_folder = "res/", plot_folder = "figs/"):
+   t0 = time.time()
+   k = j//30
+   i = j%30
+   name = "smart_%s_waste_%s_%.2f_%s"%(sim_params["smart"],sim_params["waste"],prs[k],i)
+   sim_params["planting_rate"] = prs[k]  
+   
+   res = ics.sim(sim_params, sim_params["smart"],  sim_params["waste"], svg_folder+name+".json")
+   sim_data = json.load(open(svg_folder+name+".json"))
+   ut.plot_field(sim_data["plants"], sim_data["sim_params"], [0,8000], plot_folder+name+".png")
+   print(time.time()-t0, "s for pr = %.2f %s %s %s"%(prs[k],sim_params["smart"],sim_params["waste"], i))
+   return 0
+
 
 #one_run_test()
 
@@ -32,19 +47,20 @@ def one_run(pr, sim_params, name = "test", svg_folder = "res/", plot_folder = "f
 sim_params = json.load(open('default.json'))
 sim_params["N"] = 4000
 
-prs_nowaste = np.linspace(.05,5,20)
-prs_waste = np.linspace(.05,250,20)
+#prs_nowaste = np.linspace(.05,5,20)
+prs_nowaste = np.linspace(5, 6, 20)
+prs_waste = np.linspace(.05, 250, 20)
 
-vals = [False, True]
+#vals = [False, True]
+vals=[True]
 
 for smart in vals:
     sim_params["smart"] = smart
 
-    sim_params["waste"] = True       
-    for pr in prs_waste:
-       Parallel(n_jobs=30)(delayed(one_run)(pr, svg_folder = "res/", name = "smart_%s_waste_%s_%.2f_%s"%(smart,waste,pr,i), plot_folder = "figs/") for i in range(30))
+    #sim_params["waste"] = True       
+    #for pr in prs_waste:
+    #   Parallel(n_jobs=30)(delayed(one_run)(pr, sim_params, svg_folder = "res/", name = "smart_%s_waste_%s_%.2f_%s"%(sim_params["smart"], sim_params["waste"],pr,i), plot_folder = "figs/") for i in range(30))
 
     sim_params["waste"] = False
-    for pr in prs_nowaste:
-       Parallel(n_jobs=30)(delayed(one_run)(pr, svg_folder = "res/", name = "smart_%s_waste_%s_%.2f_%s"%(smart,waste,pr,i), plot_folder = "figs/") for i in range(30))
+    Parallel(n_jobs=30)(delayed(one_run_j)(j, prs_nowaste, sim_params, svg_folder = "res/", plot_folder = "figs/") for j in range(600))
 
